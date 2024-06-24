e80b856210c6da56911e7a3778bc18c3f9897590
Chia-Wen Kao <chiawen.kao@uqconnect.edu.au>
2024-06-24 15:05:17 +0800

    Add the loading animation.

---
+++ js/site_interaction.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // hide main content
+    var header = document.getElementsByTagName('header')[0];
+    var footer = document.getElementById('footer');
+
+    // check if the element is exist
+    if (header) {
+        header.style.display = 'none';
+    }
+    if (footer) {
+        footer.style.display = 'none';
+    }
+
+    header.style.display = 'none';
+    footer.style.display = 'none';
+
+    // show the text carousel with greetings
+    const greetings = ['Hello!', '你好！'];
+    var loadingScreen = document.getElementById('loadingScreen');
+    var textCarousel = document.getElementById('textCarousel');
+    var currentGreetingIndex = 0;
+
+    function displayNextGreeting() {
+        textCarousel.innerHTML = greetings[currentGreetingIndex];
+        if (currentGreetingIndex < greetings.length) {
+            currentGreetingIndex = currentGreetingIndex + 1;
+            setTimeout(displayNextGreeting, 1000)  // display each greeting for 0.5 secs
+        } else if (currentGreetingIndex == greetings.length) {
+            loadingScreen.style.display = 'none';
+            header.style.display = 'block';
+            footer.style.display = 'block';
+        }
+    }
+
+    // start displaying greetings
+    displayNextGreeting();  // first round
+});
