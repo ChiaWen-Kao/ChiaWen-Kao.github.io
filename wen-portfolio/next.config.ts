b76ab6141e691d2386ed5dd3b9e1333891b594da
Wen Kao <chiawenkao23@gmail.com>
2025-09-02 16:42:41 +1000

    Generate the static file for GitHub page

--- wen-portfolio/next.config.ts
+++ wen-portfolio/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",  // 02092025 Static export for GitHub pages
+  basePath: "/wen-portfolio",  // 02092025 Base path for GitHub pages
 };
 
 export default nextConfig;
