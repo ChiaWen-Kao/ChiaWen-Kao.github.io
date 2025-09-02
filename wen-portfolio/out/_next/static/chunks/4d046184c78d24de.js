9a6e26cb5007fbf8b71c6496084798dd8a5ad60d
Wen Kao <chiawenkao23@gmail.com>
2025-09-02 17:00:58 +1000

    Remove basePath for a GitHub user page

--- wen-portfolio/out/_next/static/chunks/4d046184c78d24de.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/de78c933e783f3be.js",
-  "static/chunks/turbopack-f671d5d9de7d5c54.js"
-])
--- wen-portfolio/out/_next/static/chunks/d49059dbde5db1ff.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/de78c933e783f3be.js",
-  "static/chunks/turbopack-fc214523e9785f2e.js"
-])
