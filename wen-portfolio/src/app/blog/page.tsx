f3840222374e90b6a513118e1d570396ac46e173
Wen Kao <chiawenkao23@gmail.com>
2025-09-24 13:40:47 +1000

    Mod: Add responsive layout on blog page

--- wen-portfolio/src/app/blog/page.tsx
+++ wen-portfolio/src/app/blog/page.tsx
@@ -24,13 +24,13 @@ export default function Blog() {
 
   return (
     <section
-      className="flex flex-col items-center justify-center text-foreground font-montserrat text-center font-bold text-2xl"
+      className="flex flex-col items-center justify-center"
       style={{ minHeight: minH }}
     >
-      <div className="mb-10">
-        <img src="/imgs/constructing.svg" className="w-130 h-auto" />
+      <div className="mb-8 md:mb-10">
+        <img src="/imgs/constructing.svg" className="w-70 md:w-130 h-auto" />
       </div>
-      <p>
+      <p className="text-foreground font-montserrat text-center font-bold text-md md:text-2xl">
         This space will be filled with insights, stories, and projects soon.{" "}
         <br />
         Stay tuned!
