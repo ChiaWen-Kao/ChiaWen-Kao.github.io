98723f1d38cc7208f971009f3c5dba44c89af63d
Wen Kao <chiawenkao23@gmail.com>
2025-09-26 14:48:50 +1000

    Mod: Revise the content of copd work and add responsive

--- wen-portfolio/dist/server/chunks/ssr/src_91ef6913._.js
+++ wen-portfolio/dist/server/chunks/ssr/src_91ef6913._.js
@@ -425,7 +425,7 @@ function Home() {
         };
     }, []);
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-        className: "sm:px-4 md:px-2 md:px-30",
+        className: "px-4 md:px-2 md:px-30",
         children: [
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$textHero$2e$tsx__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["default"], {}, void 0, false, {
                 fileName: "[project]/src/app/page.tsx",
--- wen-portfolio/src/app/page.tsx
+++ wen-portfolio/src/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="sm:px-4 md:px-2 md:px-30">
+    <div className="px-4 md:px-2 md:px-30">
       {/* Hero Section */}
       <Hero />
 
